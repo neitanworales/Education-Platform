dae2160cbb9dc1da0b2a98d8ac506588cfa55f09
Natán Morales <pedro.morales@softtek.com>
2024-03-01 07:52:41 -0600

    fixed

--- education-platform-app/src/app/components/admin/temas/temas.component.ts
+++ education-platform-app/src/app/components/admin/temas/temas.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TemaDao } from 'src/app/api/dao/admin/TemaDao';
 import { Tema } from 'src/app/models/Tema';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
 @Component({
@@ -10,17 +10,9 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./temas.component.scss']
 })
 export class TemasComponent {
-  temaForm = this.formBuilder.group({
-    id: [''],
-    titulo: ['', Validators.required],
-    descripcion: [''],
-    presentador: [''],
-    categoria: [''],
-    estatus: [''],
-    fecha_creacion: [''],
-    fecha_updated: [''],
-    deleted_date: [''],
-  });
+  
+  temaForm = this.initForm();
+
   temas?: Tema[];
 
   constructor(
@@ -41,7 +33,35 @@ export class TemasComponent {
     );
   }
 
+  loadOnModal(tema: Tema){
+    this.temaForm.patchValue({
+      isUpdate : true,
+      id: tema.id?.toString(),
+      titulo: tema.titulo?.toString(),
+      descripcion: tema.descripcion?.toString(),
+      presentador: tema.presentador?.toString(),
+      categoria: tema.categoria?.toString(),
+      estatus: tema.estatus?.toString(),
+      fecha_creacion: tema.fecha_creacion?.toString(),
+      fecha_updated: tema.fecha_updated?.toString(),
+    });
+  }
+
   onSubmit(){
     console.warn(this.temaForm.controls['titulo'].value);
+  }
+
+  initForm(){
+    return this.formBuilder.group({
+      isUpdate: [false],
+      id: [''],
+      titulo: ['', Validators.required],
+      descripcion: [''],
+      presentador: [''],
+      categoria: [''],
+      estatus: [''],
+      fecha_creacion: [''],
+      fecha_updated: [''],
+    });
   }
 }
